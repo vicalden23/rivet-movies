ad9738b67a9dea77dd9dc3a5920f1d2e3987ed59
vic.alden23 <vic.alden23@gmail.com>
2018-08-28 20:11:23 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './containers/App'
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(
+	<App />, 
+	document.getElementById('root'));
+registerServiceWorker();
