3223c0a4f27bb94d0bfd86c4feb9a4384f126567
vic.alden23 <vic.alden23@gmail.com>
2018-08-29 18:59:13 -0700

    Add radio buttons for filtering movies

--- src/components/MovieFilters.jsx
+++ src/components/MovieFilters.jsx
@@ -5,10 +5,11 @@ import {
   Grid,
   Row,
   Col,
-  FormGroup,
-  FormControl,
   Button,
-  Radio
+  ControlLabel,
+  ButtonToolbar,
+  ToggleButton,
+  ToggleButtonGroup,
 } from 'react-bootstrap';
 
 import { filterMovies } from '../redux/actions/movieActions';
@@ -16,62 +17,128 @@ import { filterMovies } from '../redux/actions/movieActions';
 import '../containers/App.css';
 
 class MovieFilters extends Component {
+  constructor() {
+    super();
 
-  //this.props.filterMovies();
+    this.state = {
+      genre: '',
+      year: '',
+      mpaa: '',
+    };
+  }
+
 
   handleSubmit(ev) {
-    ev.preventDefault()
-    this.genreGroup.value
+    ev.preventDefault();
+    this.props.filterMovies(this.state);
+  }
+
+  handleGenreChange(genre) {
+    this.setState({
+      genre,
+    });
+  }
+
+  handleYearChange(year) {
+    this.setState({
+      year,
+    });
+  }
+
+  handleMpaaChange(mpaa) {
+    this.setState({
+      mpaa,
+    });
   }
 
   render() {
-    const { movies } = this.props.movies;
+    const genres = ['Crime', 'Comedy', 'Drama', 'Action/Adventure', 'Thriller', 'Science Fiction/Fantasy', 'Family', 'Romance', 'Animation', 'Mystery', 'Horror'];
+
+    const years = ['<1980', '1980-1989', '1990-1999', '2000-2009', '>2009'];
+
+    const mpaas = ['G', 'PG', 'PG-13', 'R'];
     return (
-      <div className='filter-container'>
+      <div className="filter-container">
         <form>
+          <ControlLabel>Genre</ControlLabel>
+          <ButtonToolbar>
+            <ToggleButtonGroup
+              type="radio"
+              name="genre"
+              defaultValue=""
+              onChange={this.handleGenreChange.bind(this)}
+              className="radio-group"
+            >
+              <ToggleButton className="filter-radio-button" value="">All</ToggleButton>
+              {
+                genres.map((genre, i) => (
+                  <ToggleButton key={`genre${i}`} className="filter-radio-button" value={genre}>{genre}</ToggleButton>
+                ))
+              }
+            </ToggleButtonGroup>
+          </ButtonToolbar>
           <Grid>
             <Row>
-              <Col xs={6} md={4}>
-                <h5>
-                  Genre:
-                </h5>
-                <Radio name="genreGroup" inline>Crime</Radio>
-                <Radio name="genreGroup" inline>Drama</Radio>
-                <Radio name="genreGroup" inline>Comedy</Radio>
-                <Radio name="genreGroup" inline>Comedy</Radio>
-                <Radio name="genreGroup" inline>Comedy</Radio>
-                <Radio name="genreGroup" inline>Comedy</Radio>
+              <Col md={7} mdPush={7}>
+                <ControlLabel>Year</ControlLabel>
+                <ButtonToolbar>
+                  <ToggleButtonGroup
+                    type="radio"
+                    name="year"
+                    defaultValue=""
+                    onChange={this.handleYearChange.bind(this)}
+                    className="radio-group"
+                  >
+                    <ToggleButton className="filter-radio-button" value="">All</ToggleButton>
+                    {
+                      years.map((year, i) => (
+                        <ToggleButton key={`years${i}`} className="filter-radio-button" value={year}>{year}</ToggleButton>
+                      ))
+                    }
+                  </ToggleButtonGroup>
+                </ButtonToolbar>
               </Col>
 
-              <Col xs={6} md={4}>
-                <h5>
-                  Year:
-                </h5>
-              </Col>
-              
-              <Col xs={6} md={4}>
-                <h5>
-                  MPAA Rating:
-                </h5>
+              <Col md={5} mdPull={5}>
+                <ControlLabel>MPAA Rating</ControlLabel>
+                <ButtonToolbar>
+                  <ToggleButtonGroup
+                    type="radio"
+                    name="mpaa"
+                    defaultValue=""
+                    onChange={this.handleMpaaChange.bind(this)}
+                    className="radio-group"
+                  >
+                    <ToggleButton className="filter-radio-button" value="">All</ToggleButton>
+                    {
+                      mpaas.map((mpaa, i) => (
+                        <ToggleButton key={`mpaas${i}`} className="filter-radio-button" value={mpaa}>{mpaa}</ToggleButton>
+                      ))
+                    }
+                  </ToggleButtonGroup>
+                </ButtonToolbar>
               </Col>
             </Row>
           </Grid>
-          <Button
-            className='submit-button'
-            type='submit'
-            onClick={this.handleSubmit.bind(this)}> 
-            PLACE ORDER
-          </Button>
+          <div className="button-container">
+            <Button
+              className="submit-filter-button"
+              type="submit"
+              onClick={this.handleSubmit.bind(this)}
+            >
+              APPLY FILTERS
+            </Button>
+          </div>
         </form>
       </div>
     );
   }
 }
 
-function mapStateToProps({movies}) {
+function mapStateToProps({ movies }) {
   return {
-    movies: movies
-  }
+    movies,
+  };
 }
 
-export default connect(mapStateToProps, {filterMovies})(MovieFilters);
+export default connect(mapStateToProps, { filterMovies })(MovieFilters);
