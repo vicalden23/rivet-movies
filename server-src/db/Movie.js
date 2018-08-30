9062827d5e53ffa2f1efd01a1f333510ba57a387
vic.alden23 <vic.alden23@gmail.com>
2018-08-29 18:58:00 -0700

    Fix lint issues

--- server-src/db/Movie.js
+++ server-src/db/Movie.js
@@ -1,24 +1,24 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-//connect to db
+// connect to db
 mongoose.connect('mongodb://vickialden123:vickialden123@ds129762.mlab.com:29762/rivet', { useNewUrlParser: true });
-let db = mongoose.connection;
-db.once('open', function() {
+const db = mongoose.connection;
+db.once('open', () => {
   console.log('Mongo connection open on ds129762.mlab.com:29762/rivet');
 });
 
-//define schema
-let movieSchema = new mongoose.Schema({
-	title: {type: String},
-	mpaa_rating: {type: String},
-	director: {type: String},
-	realease_year: {type: Number},
-	genres: {type: Array},
-	total_voters: {type: Number, default: 0},
-	average_rating: {type: Number, default: 0},
-	comments: [String]
-})
+// define schema
+const movieSchema = new mongoose.Schema({
+  title: { type: String },
+  mpaa_rating: { type: String },
+  director: { type: String },
+  realease_year: { type: Number },
+  genres: { type: Array },
+  total_voters: { type: Number, default: 0 },
+  average_rating: { type: Number, default: 0 },
+  comments: [String],
+});
 
-let Movie = mongoose.model('Movie', movieSchema, 'movies')
+const Movie = mongoose.model('Movie', movieSchema, 'movies');
 
 module.exports = Movie;
